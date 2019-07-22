b8407498c56913e7d7d20c8a10cc151fb3fef6d1
liurui0707 <1793308568@qq.com>
2019-07-22 16:48:48 +0800

    创建一个描述图书信息的类并测试。类中应保存有图书的书号、标题、作者、出版社、价格等信息。 1）定义图书类Book，Book类中包含isbn(书号)、title（标题）、author(作者)、press（出版社）、price（价格）等私有字段。由于对一本书来说，书号是唯一的，因此，isbn字段应声明为只读的。 2）为Book类中的每个字段定义相应的属性，由于isbn字段只读的，其相应属性也应该是只读的。 3）为Book类定义两个构造函数，其中，一个构造函数将所有字段都初始化为用户指定的值，另一个构造函数只要求用户指定有关书号的信息，它将调用上一个构造函数初始化对象，初始化时，价格取0,除书号的其他信息取“未知”。 4）为Book类定义方法Show，Show方法用于显示图书的所有信息。 5）编写Main方法测试Book类，Main方法中分别使用上述两个构造函数创建Book对象。

---
+++ Library.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        //定义图书isbn、标题、作者、出版社、价格等私有数据成员
+        private readonly string isbn;
+        private string title;
+        private string author;
+        private string press;
+        private double price;
+
+        //定义isbn属性只读
+        public string Isbn
+        {
+            get
+            {
+                return isbn;
+            }
+        }
+
+        //定义书名属性
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (title != value)
+                    title = value;
+            }
+        }
+
+        //定义作者属性
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                if (author != value)
+                    author = value;
+            }
+        }
+
+        //定义出版社属性
+        public string Press
+        {
+            get
+            {
+                return press;
+            }
+            set
+            {
+                if (press != value)
+                    press = value;
+            }
+        }
+
+        //定义价格属性
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (price != value)
+                {
+                    price = value;
+                }
+            }
+        }
+
+        //定义无参构造函数
+        public Book(){
+            title = "未知";
+            author = "未知";
+            press = "未知";
+            price = 0;
+            isbn = "未知";
+        }
+
+        //定义有参构造函数
+        public Book(string isbn):this()
+        {
+            this.isbn = isbn;
+        }
+
+        //定义成员方法
+        public void Show()
+        {
+            Console.WriteLine("书号：{0}",isbn);
+            Console.WriteLine("书名：{0}", title);
+            Console.WriteLine("作者：{0}", author);
+            Console.WriteLine("出版社：{0}", press);
+            Console.WriteLine("价格：{0}", price);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("第一个构造函数：");
+            Book book1 = new Book();
+            book1.Show();
+            Console.WriteLine("");
+
+            Console.WriteLine("第二个构造函数：\n请输入图书的isbn：");      
+            Book book2 = new Book(Console.ReadLine());
+            book2.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
