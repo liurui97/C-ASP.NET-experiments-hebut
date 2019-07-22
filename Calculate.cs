ad522a7e7a3cc2e472754f627c6c4fd70e00e55b
liurui0707 <1793308568@qq.com>
2019-07-22 17:04:40 +0800

    定义一个含有两个整型参数名叫Calculation返回类型为double的委托，分别实现两个匹配的求和、求平均值的方法，并在主函数中测试它。

---
+++ Calculate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    //定义一个委托类型
+    public delegate double Calculation(int first,int second);
+
+    class DeleTest
+    {
+        //计算两数之和
+        public double Add(int first,int second)
+        {
+            return first + second;            
+        }
+
+        //计算两数平均值
+        public double Average(int first,int second)
+        {
+            return (first + second) / 2.0;
+        }
+    }
+
+    public class Result
+    {
+        static void Main()
+        {
+            Console.WriteLine("请输入第一个数的值：");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入第二个数的值：");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            DeleTest deletest = new DeleTest();
+            Calculation cal1 = new Calculation(deletest.Add);
+            Console.WriteLine("{0}与{1}的和为：{2}", first, second, cal1(first,second));
+
+            Calculation cal2 = new Calculation(deletest.Average);
+            Console.WriteLine("{0}与{1}的平均值为：{2}", first, second, cal2(first,second));
+        }
+    }
+}
