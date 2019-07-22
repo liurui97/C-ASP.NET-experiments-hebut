9afd4bab80ee50adaf145760cc82e11ccdaeadb3
liurui0707 <1793308568@qq.com>
2019-07-22 16:46:16 +0800

    编写一个控制台应用程序，程序中有两个类定义，一个是创建程序时系统自动创建的类Class1，一个是用户自定义的Student类，要求该类包含私有实例字段：学号(字符串)、姓名（字符串）、成绩（double）以及私有静态字段：学生人数、学生总成绩、学生平均成绩，具有有参构造方法、显示学生个人信息的公有实例方法和显示学生人数、总成绩及平均成绩的公有静态方法。在Class1类的Main方法中完成以下功能： 1）从键盘上依次输入三个学生的个人信息（学号、姓名、成绩）。 2）统计全部学生的人数、总成绩和平均成绩。 3）打印学生们的个人信息及全部学生的人数、总成绩和平均成绩。

---
+++ StudentGrade.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGrade
+{
+    class Student
+    {
+        private string stuno;
+        private string name;
+        private double grade;
+        private static int studentNumber = 0;
+        private static double sumGrade = 0;
+        private static double avgGrade = 0;
+
+        public Student(string stuno,string name,double grade){
+            this.stuno=stuno;
+            this.name=name;
+            this.grade=grade;
+            studentNumber++;
+            sumGrade += grade;
+        }
+
+        public void studentInfo()
+        {
+            Console.WriteLine("学号：{0}", stuno);
+            Console.WriteLine("姓名：{0}", name);
+            Console.WriteLine("成绩：{0}", grade);
+        }
+
+        public static void sumInfo()
+        {
+            Console.WriteLine("学生总人数：{0}", studentNumber);
+            Console.WriteLine("学生总成绩：{0}", sumGrade);
+            avgGrade=sumGrade/studentNumber;
+            Console.WriteLine("平均成绩{0}", avgGrade);
+        }
+    }
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Student[] student=new Student[3];
+            string stuno;
+            string name;
+            double grade;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("请输入第{0}个学生的学号",i+1);
+                stuno = Console.ReadLine();
+                Console.WriteLine("请输入第{0}个学生的姓名",i+1);
+                name = Console.ReadLine();
+                Console.WriteLine("请输入第{0}个学生的成绩", i+1);
+                grade = Convert.ToDouble(Console.ReadLine());
+                student[i] = new Student(stuno, name, grade);
+            }
+            for (int j = 0; j <3 ; j++)
+            {
+                student[j].studentInfo();
+            }
+            Student.sumInfo();
+            Console.ReadLine();
+        }
+    }
+}
