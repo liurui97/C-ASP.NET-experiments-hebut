8a271b5f8ddbf980ddff340f39844616f5584faf
liurui0707 <1793308568@qq.com>
2019-07-22 17:28:28 +0800

    Session对象应用方法 1）使用Session对象进行页面间的数据传递及页面访问控制，设计Login.aspx页面和Display.aspx页面。 2）Login.aspx页面中，将一个Label控件的内容置为“Session对象应用举例！”，当输入用户名和密码后，单击“登录”按钮，页面跳转到Display.aspx页面，并利用Session对象将Login.aspx页面中Lable控件的内容传递到Display.aspx页面，并在其页面中的Label控件中显示出来。

---
+++ Session/Display.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Display : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["name"] == null || Session["pass"] == null)
+        {
+            Response.Write("<script language=javascript>window.alert('为了您的安全，请先登录!');window.location.href=('Login.aspx');</scrip>");
+        }
+        else
+        {
+            label2.Text = Session["xianshi"].ToString();
+        }
+    }
+}
---
+++ Session/Login.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void submit_Click(object sender, EventArgs e)
+    {
+        Session["name"] = this.UserName.Text.ToString();
+        Session["pass"] = this.PassWord.Text.ToString();
+        Session["xianshi"] = this.label1.Text.ToString();
+        Response.Redirect("Display.aspx");
+    }
+}
