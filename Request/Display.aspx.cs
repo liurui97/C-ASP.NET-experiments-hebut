2b84ab2c093b2b780edae8c1cb4a09bd9bbe5b1f
liurui0707 <1793308568@qq.com>
2019-07-22 17:23:47 +0800

    Request 对象应用方法 1）用Request对象获取表单数据、客户端浏览器的信息及服务器端环境变量。 2）设计两个页面：Login.aspx和Showmassage.aspx，在Login.aspx中输入用户名和密码，然后利用Request对象获取用户名和密码表单数据，在Showmassage.aspx页面中显示出来，并在此页面显示出客户端浏览器的信息和服务器端环境变量。

---
+++ Request/Display.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Display : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("获取表单数据并显示：" + "<br>");
+        Response.Write("用户名：" + Request.QueryString["name"] + "<br>");
+        Response.Write("密码：" + Request.QueryString["pass"] + "<br>");
+        Response.Write("获取用户浏览器信息并显示：" + "<br>");
+        HttpBrowserCapabilities bc = Request.Browser;
+        Response.Write("浏览器=" + bc.Browser + "<br>");
+        Response.Write("版本=" + bc.Version + "<br>");
+        Response.Write("使用平台=" + bc.Platform + "<br>");
+        Response.Write("是否支持测试版=" + bc.Beta + "<br>");
+        Response.Write("是否为16位环境"+bc.Win16+"<br>");
+        Response.Write("是否为32位环境"+bc.Win32+"<br>");
+        Response.Write("是否支持框架="+bc.Frames+"<br>");
+        Response.Write("是否支持表格"+bc.Tables+"<br>");
+        Response.Write("是否支持Cookie"+bc.Cookies+"<br>");
+        Response.Write("是否支持VB Script"+bc.VBScript+"<br>");
+        Response.Write("是否支持Java Script"+bc.JavaScript+"<br>");
+        Response.Write("是否支持Java Applets"+bc.JavaApplets+"<br>");
+        Response.Write("是否支持ActiveX Controls="+bc.ActiveXControls+"<br>");
+        Response.Write("获取服务器端环境并显示"+"<br>");
+        Response.Write("当前网页虚拟路径"+Request.ServerVariables["URL"]+"<br>");
+        Response.Write("实际路径"+Request.ServerVariables["PATH_TRANSLATED"]+"<br>");
+        Response.Write("服务器名或IP"+Request.ServerVariables["SERVER_NAME"]+"<br>");
+        Response.Write("服务器连接端口" + Request.ServerVariables["SERVER_PORT"]+"<br>");
+        Response.Write("HTTP版本" + Request.ServerVariables["SERVER_PORTOCOL"]);
+        Response.Write("客户主机名" + Request.ServerVariables["REMOTE_HOST"] + "<br>");
+        Response.Write("浏览器" + Request.ServerVariables["HTTP_USER_AGENT"] + "<br>");
+    }
+}
---
+++ Request/Login.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="login.css"/>
+</head>
+<body>
+    <div id="login_frame">
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td colspan="3" style="text-align:center">用户登录</td>
+            </tr>
+            <tr>
+                <td style="width:128px"><asp:Label CssClass="label_input" runat="server">用户名：</asp:Label></td>
+                <td colspan="2" style="width:183px">
+                    <asp:TextBox ID="UserName" runat="server" CssClass="text_field"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width:128px"><asp:Label CssClass="label_input" runat="server">密&nbsp;码：</asp:Label></td>
+                <td colspan="2" style="width:183px">
+                    <asp:TextBox ID="PassWord" runat="server" TextMode="Password" CssClass="text_field"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align:center">
+                    <asp:Button ID="submit" runat="server" Text="登陆" OnClick="submit_Click" CssClass="btn_login"/>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+        </div>
+</body>
+</html>
