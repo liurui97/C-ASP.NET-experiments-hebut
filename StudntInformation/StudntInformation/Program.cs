1f732f1eef47683f46d2ea2c98ebe0e0c586d019
liurui0707 <1793308568@qq.com>
2019-07-22 16:43:15 +0800

    编写一个控制台应用程序，程序中有两个类定义，一个是创建程序时系统自动创建的类Class1，一个是用户自定义的Student类，要求该类包含私有字段：学号(字符串)、姓名（字符串）和性别（字符），具有三个属性：学号（读写）、姓名（只读）、性别（读写），具有有参构造方法、具有同时显示学生个人信息的方法。在Class1类的Main方法中完成以下功能： 1）从键盘上输入一个学生的个人信息（学号、姓名、性别）。 2）修改该学生的学号和性别。 3）打印修改前后该学生的个人信息。

---
+++ StudntInformation/StudntInformation/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudntInformation
+{
+    class Student
+    {
+        //定义学生类字段
+        private string stuno;
+        private string name;
+        private string sex;
+
+        public string Stuno
+        {
+            get
+            {
+                return stuno;
+            }
+            set
+            {
+                stuno = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string Sex
+        {
+            get
+            {
+                return sex;
+            }
+            set
+            {
+                sex = value;
+            }
+        }
+
+        public Student(string stuno, string name, string sex)
+        {
+            this.stuno = stuno;
+            this.name = name;
+            this.sex = sex;
+        }
+
+        public void StudentInfo()
+        {
+            Console.WriteLine("学生信息如下");
+            Console.WriteLine("学号：{0}", stuno);
+            Console.WriteLine("姓名：{0}", name);
+            Console.WriteLine("性别：{0}", sex);
+        }
+    }
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入学生的学号：");
+            string stuno = Console.ReadLine();
+            Console.WriteLine("请输入学生的姓名：");
+            string name = Console.ReadLine();
+            Console.WriteLine("请输入学生的性别：");
+            string sex = Console.ReadLine();
+
+            Student student = new Student(stuno, name, sex);
+            student.StudentInfo();
+            Console.WriteLine();
+
+            //修改学生信息
+            student.Stuno = "164574";
+            student.Sex = "男";
+            
+            Console.WriteLine("修改后的学生信息为：");
+            student.StudentInfo();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
